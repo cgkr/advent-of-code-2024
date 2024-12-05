ccdea4227681dfe0851fd5a57126278e5e0d2564
Christos Gkerekos <christos@gkerekos.net>
2024-12-05 18:44:03 +0000

    Add part b test

--- day01/test_sol.py
+++ day01/test_sol.py
@@ -4,3 +4,8 @@ from .sol import solve_part_a, solve_part_b
 def test_solve_part_a():
     part_a_expected_output = 11
     assert solve_part_a("test_input.txt") == part_a_expected_output
+
+
+def test_solve_part_b():
+    part_b_expected_output = 31
+    assert solve_part_b("test_input.txt") == part_b_expected_output
