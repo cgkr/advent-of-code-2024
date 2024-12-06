d5303dee7026afef7e096aed122013e7f747f919
Christos Gkerekos <christos@gkerekos.net>
2024-12-06 16:10:37 +0000

    Add parameterized test for filter_for_conditionals and update imports

--- day03/test_sol.py
+++ day03/test_sol.py
@@ -1,4 +1,6 @@
-from .sol import solve_part_a, solve_part_b
+import pytest
+
+from .sol import filter_for_conditionals, solve_part_a, solve_part_b
 
 
 def test_solve_part_a():
@@ -9,3 +11,23 @@ def test_solve_part_a():
 def test_solve_part_b():
     part_b_expected_output = 48
     assert solve_part_b("test_input_b.txt") == part_b_expected_output
+
+
+@pytest.mark.parametrize(
+    "input_str, expected_output",
+    [
+        (
+            "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))",
+            "xmul(2,4)&mul[3,7]!^?mul(8,5))",
+        ),
+        ("don't()remove_thisdo()keep_this", "keep_this"),
+        ("keep_thisdon't()remove_thisdo()", "keep_this"),
+        (
+            "don't()remove_thisdodon't()remove_this_too_do()keep_thisdo()",
+            "keep_thisdo()",
+        ),
+        ("keep_thisdon't()remove_this", "keep_this"),
+    ],
+)
+def test_filter_for_conditionals(input_str, expected_output):
+    assert filter_for_conditionals(input_str) == expected_output
