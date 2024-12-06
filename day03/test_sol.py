2fdcd7819887cbbc0579fe44851ca3da3b2c3b14
Christos Gkerekos <christos@gkerekos.net>
2024-12-06 14:06:49 +0000

    Add test input file and update test cases for part A and part B

--- day03/test_sol.py
+++ day03/test_sol.py
@@ -2,10 +2,10 @@ from .sol import solve_part_a, solve_part_b
 
 
 def test_solve_part_a():
-    part_a_expected_output = ...
+    part_a_expected_output = 161
     assert solve_part_a("test_input.txt") == part_a_expected_output
 
 
 def test_solve_part_b():
-    part_b_expected_output = ...
-    assert solve_part_b("test_input.txt") == part_b_expected_output
+    part_b_expected_output = 48
+    assert solve_part_b("test_input_b.txt") == part_b_expected_output
