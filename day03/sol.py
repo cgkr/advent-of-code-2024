6d4f57a2b1b7eea1b85827d403ff7a0d46c5545a
Christos Gkerekos <christos@gkerekos.net>
2024-12-06 13:21:31 +0000

    Initial commit

---
+++ day03/sol.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+
+def solve_part_a(filename: str = "input.txt") -> int: ...
+
+
+def solve_part_b(filename: str = "input.txt") -> int: ...
+
+
+if __name__ == "__main__":
+    result = solve_part_a(filename="input.txt")
+    print(str(result))
